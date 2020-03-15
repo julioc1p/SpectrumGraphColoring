e2f5847d2040a9468ecfd1bb6dd87c8de08238b3
Julio Cesar Sanchez Garcia <julio.c.hxh@gmail.com>
2020-03-15 15:13:04 -0400

    Adapting graph class

--- graph2.py
+++ graph2.py
@@ -32,32 +32,30 @@ class Graph(object):
 
     def add_edge(self, edge):
         """ assumes that edge is of type set, tuple or list; 
-            between two vertices can be multiple edges! 
+            the edges are undirected and
+            between two vertices cannot be multiple edges! 
         """
-        edge = set(edge)
-        vertex1 = edge.pop()
-        if edge:
-            # not a loop
-            vertex2 = edge.pop()
-        else:
-            # a loop
-            vertex2 = vertex1
-        if vertex1 in self.__graph_dict:
-            self.__graph_dict[vertex1].append(vertex2)
+        vertex1, vertex2 = edge
+        if vertex1 in self.__graph_dict :
+            if not vertex2 in self.__graph_dict[vertex1]:
+                self.__graph_dict[vertex1].append(vertex2)
         else:
             self.__graph_dict[vertex1] = [vertex2]
+        if vertex2 in self.__graph_dict :
+            if not vertex1 in self.__graph_dict[vertex2]:
+                self.__graph_dict[vertex2].append(vertex1)
+        else:
+            self.__graph_dict[vertex2] = [vertex1]
 
     def __generate_edges(self):
         """ A static method generating the edges of the 
             graph "graph". Edges are represented as sets 
-            with one (a loop back to the vertex) or two 
-            vertices 
+            with two vertices 
         """
         edges = []
         for vertex in self.__graph_dict:
             for neighbour in self.__graph_dict[vertex]:
-                if {neighbour, vertex} not in edges:
-                    edges.append({vertex, neighbour})
+                edges.append((vertex, neighbour))
         return edges
 
     def __str__(self):
@@ -69,80 +67,10 @@ class Graph(object):
             res += str(edge) + " "
         return res
 
-    def find_isolated_vertices(self):
-        """ returns a list of isolated vertices. """
-        graph = self.__graph_dict
-        isolated = []
-        for vertex in graph:
-            print(isolated, vertex)
-            if not graph[vertex]:
-                isolated += [vertex]
-        return isolated
-
-    def find_path(self, start_vertex, end_vertex, path=[]):
-        """ find a path from start_vertex to end_vertex 
-            in graph """
-        graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return path
-        if start_vertex not in graph:
-            return None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_path = self.find_path(vertex, 
-                                               end_vertex, 
-                                               path)
-                if extended_path: 
-                    return extended_path
-        return None
-    
-
-    def find_all_paths(self, start_vertex, end_vertex, path=[]):
-        """ find all paths from start_vertex to 
-            end_vertex in graph """
-        graph = self.__graph_dict 
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return [path]
-        if start_vertex not in graph:
-            return []
-        paths = []
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_paths = self.find_all_paths(vertex, 
-                                                     end_vertex, 
-                                                     path)
-                for p in extended_paths: 
-                    paths.append(p)
-        return paths
-
-    def is_connected(self, 
-                     vertices_encountered = None, 
-                     start_vertex=None):
-        """ determines if the graph is connected """
-        if vertices_encountered is None:
-            vertices_encountered = set()
-        gdict = self.__graph_dict        
-        vertices = gdict.keys() 
-        if not start_vertex:
-            # chosse a vertex from graph as a starting point
-            start_vertex = vertices[0]
-        vertices_encountered.add(start_vertex)
-        if len(vertices_encountered) != len(vertices):
-            for vertex in gdict[start_vertex]:
-                if vertex not in vertices_encountered:
-                    if self.is_connected(vertices_encountered, vertex):
-                        return True
-        else:
-            return True
-        return False
-
     def vertex_degree(self, vertex):
         """ The degree of a vertex is the number of edges connecting
-            it, i.e. the number of adjacent vertices. Loops are counted 
-            double, i.e. every occurence of vertex in the list 
-            of adjacent vertices. """ 
+            it, i.e. the number of adjacent vertices. 
+        """ 
         adj_vertices =  self.__graph_dict[vertex]
         degree = len(adj_vertices) + adj_vertices.count(vertex)
         return degree
@@ -154,16 +82,6 @@ class Graph(object):
             seq.append(self.vertex_degree(vertex))
         seq.sort(reverse=True)
         return tuple(seq)
-
-    @staticmethod
-    def is_degree_sequence(sequence):
-        """ Method returns True, if the sequence "sequence" is a 
-            degree sequence, i.e. a non-increasing sequence. 
-            Otherwise False is returned.
-        """
-        # check if the sequence sequence is non-increasing:
-        return all( x>=y for x, y in zip(sequence, sequence[1:]))
-  
 
     def delta(self):
         """ the minimum degree of the vertices """
@@ -182,51 +100,6 @@ class Graph(object):
             if vertex_degree > max:
                 max = vertex_degree
         return max
-
-    def density(self):
-        """ method to calculate the density of a graph """
-        g = self.__graph_dict
-        V = len(g.keys())
-        E = len(self.edges())
-        return 2.0 * E / (V *(V - 1))
-
-    def diameter(self):
-        """ calculates the diameter of the graph """
-        
-        v = self.vertices() 
-        pairs = [ (v[i],v[j]) for i in range(len(v)-1) for j in range(i+1, len(v))]
-        smallest_paths = []
-        for (s,e) in pairs:
-            paths = self.find_all_paths(s,e)
-            smallest = sorted(paths, key=len)[0]
-            smallest_paths.append(smallest)
-
-        smallest_paths.sort(key=len)
-
-        # longest path is at the end of list, 
-        # i.e. diameter corresponds to the length of this path
-        diameter = len(smallest_paths[-1])
-        return diameter
-
-    @staticmethod
-    def erdoes_gallai(dsequence):
-        """ Checks if the condition of the Erdoes-Gallai inequality 
-            is fullfilled 
-        """
-        if sum(dsequence) % 2:
-            # sum of sequence is odd
-            return False
-        if Graph.is_degree_sequence(dsequence):
-            for k in range(1,len(dsequence) + 1):
-                left = sum(dsequence[:k])
-                right =  k * (k-1) + sum([min(x,k) for x in dsequence[k:]])
-                if left > right:
-                    return False
-        else:
-            # sequence is increasing
-            return False
-        return True
-
    
 
 
@@ -234,7 +107,7 @@ if __name__ == "__main__":
 
     g = { "a" : ["d"],
           "b" : ["c"],
-          "c" : ["b", "c", "d", "e"],
+          "c" : ["b", "d", "e"],
           "d" : ["a", "c"],
           "e" : ["c"],
           "f" : []
@@ -245,15 +118,6 @@ if __name__ == "__main__":
 
     for node in graph.vertices():
         print(graph.vertex_degree(node))
-
-    print("List of isolated vertices:")
-    print(graph.find_isolated_vertices())
-
-    print("""A path from "a" to "e":""")
-    print(graph.find_path("a", "e"))
-
-    print("""All pathes from "a" to "e":""")
-    print(graph.find_all_paths("a", "e"))
 
     print("The maximum degree of the graph is:")
     print(graph.Delta())
@@ -267,17 +131,6 @@ if __name__ == "__main__":
     print("Degree Sequence: ")
     ds = graph.degree_sequence()
     print(ds)
-
-    fullfilling = [ [2, 2, 2, 2, 1, 1], 
-                         [3, 3, 3, 3, 3, 3],
-                         [3, 3, 2, 1, 1]
-                       ] 
-    non_fullfilling = [ [4, 3, 2, 2, 2, 1, 1],
-                        [6, 6, 5, 4, 4, 2, 1],
-                        [3, 3, 3, 1] ]
-
-    for sequence in fullfilling + non_fullfilling :
-        print(sequence, Graph.erdoes_gallai(sequence))
 
     print("Add vertex 'z':")
     graph.add_vertex("z")
