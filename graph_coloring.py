01ca0213d1ce5fb9ded5ba8b6c2bede5d9025d44
Julio Cesar Sanchez Garcia <julio.c.hxh@gmail.com>
2020-03-17 14:46:02 -0400

    Renaming GraphColoring class, and commented it.

--- graph_coloring.py
+++ graph_coloring.py
@@ -1,8 +1,34 @@
 from graph2 import Graph
+from gcd import lgcd
 
-class GraphColoring(object):
+""" A Python Class
+    A Python graph class for two Spectrum Graph Coloring problems,
+    the Threshold Spectrum Coloring problem and the Chromatic Spectrum
+    Coloring problem. This class implements some methods and 
+    bounds for these problems.
+
+    "Threshold Spectrum Coloring (TSC) problem":
+        Given a graph G and a spectrum of k colors, endowed with
+        a k × k matrix W of interferences between them, the goal is to determine
+        the minimum threshold t ∈ R ≥0 such that (G, W ) admits a k-coloring c 
+        in which the interference at every vertex is at most t.
+
+    "Chromatic Spectrum Coloring (CSC) problem":
+        Given a graph G and a spectrum of colors S, endowed with a |S| × |S|
+        matrix W of interferences between them, a threshold t ∈ R ≥0 is fixed and 
+        the spectrum is let to have as size the number |V (G)| of vertices, the goal
+        being to determine the minimum number of colors k ∈ N such that (G, W ) admits
+        a k-coloring c in which the interference at every vertex is at most that 
+        threshold t.
+"""
+
+class SpectrumGraphColoring(object):
 
     def __init__(self, graph, spectrum, w, c=None):
+        """ initializes a GraphColoring object
+            "graph" is a Graph object, "spectrum" is a list
+            of colors and "w" is a dictionary of weights.
+        """
         self.__graph = graph
         self.__spectrum = spectrum
         self.__w = w
@@ -10,7 +36,7 @@ class GraphColoring(object):
 
     def set_coloring(self, c):
         """ set a coloring "c" to the vertices of self.__graph, 
-            "c" is expected to be a dict .  
+            "c" is expected to be a dict.
         """
         self.__c = c
     
@@ -45,15 +71,37 @@ class GraphColoring(object):
         return True
 
     def tsc_upper_bound(self, k):
+        """ it determites the upper bound for the tsc problem,
+            using self.__graph, self.__spectrum and the matrix self.__w,
+            and a "k" between 2 and |self.__w|.
+        """
         nnorm = self.__natural_norm()
         Delta = self.__graph.Delta()
         return (Delta*nnorm)/k
     
+    def csc_upper_bound(self, t):
+        """ it determites the upper bound for the csc problem,
+            using self.__graph, self.__spectrum, the matrix self.__w
+            and a "t" such that 
+            |self.__w|*t >= MaxDeg(self.__graph)*NormalNorm(self.__w).
+        """
+        nnorm = self.__natural_norm()
+        Delta = self.__graph.Delta()
+        if len(self.__spectrum)*t < Delta*nnorm:
+            return -1
+        w = [self.__w[i] for i in self.__w]
+        w = [item for sublist in w for item in sublist]
+        gcd_w = lgcd(w)
+        if gcd_w % t == 0:
+            return -( -(Delta*nnorm + gcd_w) // (t + gcd_w) )
+        else :
+            return -( -(Delta*nnorm + gcd_w) // (gcd_w * (t//gcd_w) + gcd_w) )
+
     def __natural_norm(self):
         """ a static method to calculate the natural norm of self.__w """
         max_row_sum = 0
         for i in self.__w:
-            row_sum = sum(self.__w[i])
+            row_sum = sum(self.__w[i].values())
             if row_sum > max_row_sum:
                 max_row_sum = row_sum
         return max_row_sum  
